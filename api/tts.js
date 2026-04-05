f118d6ca565c77fa773aada87742145ab6d589f9
Efe Erman <efee7946@gmail.com>
2026-04-05 16:45:42 +0300

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
                 },
                 body: JSON.stringify({
                     text,
-                    model_id: 'eleven_flash_v2_5',
+                    model_id: 'eleven_multilingual_v2',
                     voice_settings: { stability: 0.5, similarity_boost: 0.75 }
                 })
             }
